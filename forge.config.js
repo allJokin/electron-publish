ece8fc8638702a662d842056eec47a261cfb4e14
ttpeanuts <ttpeanuts@gmail.com>
2021-09-18 21:59:26 +0900

    publish 設定

---
+++ forge.config.js
@@ -0,0 +1,52 @@
+module.export = {
+  makers: [
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: {
+        name: "electron_publish",
+      },
+    },
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
+    },
+    {
+      name: "@electron-forge/maker-deb",
+      config: {},
+    },
+    {
+      name: "@electron-forge/maker-rpm",
+      config: {},
+    },
+  ],
+  plugins: [
+    [
+      "@electron-forge/plugin-webpack",
+      {
+        mainConfig: "./webpack.main.config.js",
+        renderer: {
+          config: "./webpack.renderer.config.js",
+          entryPoints: [
+            {
+              html: "./src/index.html",
+              js: "./src/renderer.ts",
+              name: "main_window",
+            },
+          ],
+        },
+      },
+    ],
+  ],
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        repository: {
+          owner: "allJokin",
+          name: "electron-publish",
+        },
+        prerelease: true,
+      },
+    },
+  ],
+};
